30aef9f5c51f2ef04a962cf66854e8ccf815da2c
Fabio Moreira <fabiomoreira@MacBook-Air-de-Fabio.local>
2026-07-21 10:16:34 -0300

    ui(test): adicionar download do certificado público no menu Instalar
    - Link para baixar digital-certificate.txt (certificado público)
    - Instrução: copiar para C:\Program Files\QZ Tray\override\
    - Resolve 'untrusted website' ao instalar o cert no QZ Tray do cliente

--- ebi/template/views/main.test.php
+++ ebi/template/views/main.test.php
@@ -250,6 +250,15 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download mr-1" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/></svg>
                                 QZ Tray 2.2.5 x64 (.exe)
                             </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="assets/signing/digital-certificate.txt" download="digital-certificate.txt">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-lock mr-1" viewBox="0 0 16 16"><path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56"/><path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415"/></svg>
+                                Certificado Público (.txt)
+                            </a>
+                            <h6 class="dropdown-header" style="font-size:0.65rem;color:#999;padding:4px 24px;">
+                                Copie o certificado para:<br>
+                                <code style="font-size:0.6rem;">C:\Program Files\QZ Tray\override\</code>
+                            </h6>
                         </div>
                     </div>
                     <div class="dropdown-divider"></div>
